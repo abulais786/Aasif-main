d45d2ec9273642b4aa718277ef34340e260dd231
AasifBot <ninjaworries007@gmail.com>
2024-12-18 17:54:23 +0300

    Update girl.js

--- Priyansh/commands/girl.js
+++ Priyansh/commands/girl.js
@@ -1,7 +1,7 @@
   module.exports.config = {
   name: "girl",
   version: "1.0.0",
-  hasPermssion: 0,
+  hasPermssion: 2,
   credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
   description: "Random girl picturesl",
   commandCategory: "Image",
