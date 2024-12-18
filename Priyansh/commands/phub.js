ab960a9906c9b378a3b0c0076dfb030da26511df
AasifBot <ninjaworries007@gmail.com>
2024-12-18 18:56:36 +0300

    Update phub.js

--- Priyansh/commands/phub.js
+++ Priyansh/commands/phub.js
@@ -1,7 +1,7 @@
 module.exports.config = {
 	name: "phub",
 	version: "1.0.1",
-	hasPermssion: 0,
+	hasPermssion: 2,
 	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
 	description: "Comment trên pỏnhub ( ͡° ͜ʖ ͡°)",
 	commandCategory: "Edit-img",
